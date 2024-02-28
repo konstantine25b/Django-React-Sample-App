9dc5cc3cc4df67728537529178e2a8f5485b02e8
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-28 13:59:41 +0400

    added urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app',
 ]
 
@@ -38,7 +39,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -112,3 +116,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permission.AllowAny',],
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
